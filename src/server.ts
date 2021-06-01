6e3fd83c919c710b21a17d31e776cf392fbe0a66
Guilheeeerme <guilherme_moura.10222@outlook.com>
2021-06-01 12:10:53 -0300

    config server

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import { ApolloServer } from "apollo-server";
+
+async function bootstrap() {
+  const server = new ApolloServer({});
+
+  server.listen({ port: 4100 }, console.log("Server is running"));
+}
+
+bootstrap();
